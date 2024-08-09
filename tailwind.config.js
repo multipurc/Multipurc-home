f7cd421b5f477277f81eca7a23f9edf55bda6126
multipurc <141567670+multipurc@users.noreply.github.com>
2024-08-10 01:20:29 +0600

    main

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#1b434d'
+      }
+    },
+  },
+  plugins: [],
+}
+
